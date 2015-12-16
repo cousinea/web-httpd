88bd8199825813c8c0035f8f9832c3d9a6974255
cousinea <cousinea@umich.edu>
2015-12-16 16:02:36 -0500

    Update init.sh

--- skel/init.sh
+++ skel/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-build=$(cat build)
+#build=$(cat build)
 
-sed -i -e "s|<containerid>|$(hostname)|g"  \
-       -e "s|<build>|$build|g"       \
-       /usr/share/html/index.html
+#sed -i -e "s|<containerid>|$(hostname)|g"  \
+#       -e "s|<build>|$build|g"       \
+#       /usr/share/html/index.html
 
-PASSVAL=`grep PASSWORD /password | cut -d= -f2`
+#PASSVAL=`grep PASSWORD /password | cut -d= -f2`
 
-sed -i "s/PASSWORD/$PASSVAL/g" /usr/share/html/index.html
+#sed -i "s/PASSWORD/$PASSVAL/g" /usr/share/html/index.html
 
+echo "Hi, I'm in init.sh"
 #exec nginx -g 'daemon off;'
